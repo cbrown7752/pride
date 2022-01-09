9f08623201f0bbc458c83e346a57d04711c3a177
culanndog3111 <59544410+cbrown7752@users.noreply.github.com>
2022-01-08 22:20:59 -0600

    Add files via upload

---
+++ copy.js
@@ -0,0 +1,47 @@
+function copyToClipboard1(){
+    let val = 'lgbtq-community.tk#coming-out';
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    alert('URL copied to clipboard');
+  }
+
+function copyToClipboard2(){
+    let val = 'lgbtq-community.tk#help';
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    alert('URL copied to clipboard');
+  }
+
+function copyToClipboard3(){
+    let val = 'lgbtq-community.tk#define';
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    alert('URL copied to clipboard');
+  }
